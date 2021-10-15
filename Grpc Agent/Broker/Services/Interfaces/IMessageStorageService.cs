c79951f1807cf10c83598b21d8d3bd8386ce91fd
Gabriel0307 <gabriel_zdravkovic313@mail.ru>
2021-10-15 10:05:01 +0300

    Lab1

---
+++ Grpc Agent/Broker/Services/Interfaces/IMessageStorageService.cs
@@ -0,0 +1,20 @@
+﻿using Broker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Broker.Services.Interfaces
+{
+    public interface IMessageStorageService
+    {
+        public void Add(Message message);
+
+        Message getNext();
+
+        bool isEmpty();
+
+
+
+    }
+}
